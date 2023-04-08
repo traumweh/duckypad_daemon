776ca0f67e98b65cb11d44c558ffaea653330040
traumweh <5042134-traumweh@users.noreply.gitlab.com>
2023-04-08 19:07:56 +0200

    Callback parameters: Added app_name/cmd back

--- examples/callbacks/posix.sh
+++ examples/callbacks/posix.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 profile=
+app_name=
 title=
 process_name=
 
-while getopts p:t:n: name
+while getopts p:a:t:n: name
 do
   case $name in
   p)  profile="$OPTARG";;
+  a)  app_name="$OPTARG";;
   t)  title="$OPTARG";;
   n)  process_name="$OPTARG";;
-  ?)  printf "Usage: %s: [-p profile] [-t title] [-n process_name]\n" $0
+  ?)  printf "Usage: %s: [-p profile] [-a app_name] [-t title] [-n process_name]\n" $0
       exit 2;;
   esac
 done
 
 echo "Got the following values:"
 echo "Profile: $profile"
+echo "App Name: $app_name"
 echo "Title: $title"
 echo "Process Name: $process_name"
